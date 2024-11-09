9a9cadf9c1d7bf7da6bc80e15f0f8e5e4c80c691
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-11-09 20:16:37 +0800

    更新 build.sh

--- build.sh
+++ build.sh
@@ -18,32 +18,6 @@
 #     along with Ayame PaperDoll.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-#
-#     Custom player model mod. Powered by GeckoLib.
-#     Copyright (C) 2024  CrystalNeko, HappyRespawnanchor, pertaz(Icon Designer)
-#
-#     This file is part of Ayame.
-#
-#     Ayame is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU Lesser General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     Ayame is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU Lesser General Public License for more details.
-#
-#     You should have received a copy of the GNU Lesser General Public License
-#     along with Ayame.  If not, see <https://www.gnu.org/licenses/>.
-#
-
-chmod +x gradlew
-# ./gradlew runDatagen &      # 将命令放入后台运行
-# pid=$!              # 获取该命令的PID
-# sleep 60
-# kill -9 $pid       # 60s后强制kill
-
 ./gradlew build
 mkdir result
 cp -r fabric/build/libs/* result
