6221a5231e28f753fb21520a15b6339405620f82
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-12-06 21:40:00 +0800

    修复Mixin

--- common/src/main/java/org/ayamemc/ayamepaperdoll/mixin/retexture/AbstractAbstractScrollArea.java
+++ common/src/main/java/org/ayamemc/ayamepaperdoll/mixin/retexture/AbstractAbstractScrollArea.java
@@ -23,7 +23,7 @@ package org.ayamemc.ayamepaperdoll.mixin.retexture;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.AbstractSelectionList;
+import net.minecraft.client.gui.components.AbstractScrollArea;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import org.ayamemc.ayamepaperdoll.config.view.Retextured;
@@ -32,12 +32,12 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.function.Function;
 
-@Mixin(AbstractSelectionList.class)
-public class AbstractSelectionListMixin {
+@Mixin(AbstractScrollArea.class)
+public class AbstractAbstractScrollArea {
     /**
      * This is an incomplete implementation. The background and separator/header/footer are untouched.
      */
-    @WrapOperation(method = "renderWidget", at = {
+    @WrapOperation(method = "renderScrollbar", at = {
             @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Ljava/util/function/Function;Lnet/minecraft/resources/ResourceLocation;IIII)V")
     })
     public void drawTransparentTextFieldTexture(GuiGraphics instance, Function<ResourceLocation, RenderType> function, ResourceLocation resourceLocation, int i, int j, int k, int l, Operation<Void> original) {
